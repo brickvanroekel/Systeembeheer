8061d023fea53941ae80b510905775e540ab6aff
brickvanroekel <brick.vanroekel@student.ucll.be>
2023-07-06 15:25:17 +0200

    add zone script update

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -3,13 +3,22 @@
 #named.conf.mrt-zones
 echo "zone \"$1\" {
         type master;
-        file \"/etc/bind/db.$1\.be";
+        file \"/etc/bind/db.$1.brick-vanroekel.sb.uclllabs.be\";
 };" >> /etc/bind/named.conf.mrt-zones
 
-#named.conf.local
-echo "zone \"$1\" {
-        type master;
-        file \"/etc/bind/db.$1\.be";
-};" >> /etc/bind/named.conf.local
+mkdir -p /etc/bind/zones
+touch /etc/bind/zones/db.$1.brick-vanroekel.sb.uclllabs.be
+
+echo "\$TTL 900
+\$ORIGIN brick-vanroekel.sb.uclllabs.be.
+@       IN      SOA     brick-vanroekel.sb.uclllabs.be. r0795577.sb.uclllabs.be. (
+                        1      ; Serial
+               		900    ; Refresh
+                        900    ; Retry
+                        900    ; Expire
+                        900 )  ; Negative Cache TTL
+;
+@	IN	NS	ns.brick-vanroekel.sb.uclllabs.be.
+ns	IN	A	193.191.177.138" > /etc/bind/zones/db.$1.brick-vanroekel.sb.uclllabs.be
 
 
